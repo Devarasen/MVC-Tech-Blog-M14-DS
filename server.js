2b0627870e2ed0edff654e11e6f3df8ade4ce85f
Devarasen <devarasen@gmail.com>
2023-09-11 09:41:25 +1000

    set up buttons for edit and delete

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("view engine", ".handlebars");
 const sess = {
   secret: "Super secret secret",
   cookie: {
-    maxAge: 5 * 60 * 1000,
+    maxAge: 30 * 60 * 1000,
     httpOnly: true,
     secure: false,
     sameSite: "lax",
